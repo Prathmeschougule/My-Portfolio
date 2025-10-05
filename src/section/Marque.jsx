21384422b6abd5b18c8b1d315481891eb64ffc35
Prathmesh Chougule <prathmeshuix@gmail.com>
2025-10-05 17:25:58 +0530

    star

--- src/section/Marque.jsx
+++ src/section/Marque.jsx
@@ -5,23 +5,23 @@ function Marque() {
     <div className='bg-gray-200 text-black  py-24   overflow-hidden '>
         <div className='whitespace-nowrap animate-marque font-heading  text-6x lg:text-[7vw] leading-[1]'> 
         <span className='  font-bold flex items-center gap-4'>
-            CREATE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
-            DESIGN <img src="src/assets/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
-            INSPIRE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
-            CREATE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
-            DESIGN <img src="src/assets/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
-            INSPIRE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
+            CREATE <img src="/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
+            DESIGN <img src="/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
+            INSPIRE <img src="/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
+            CREATE <img src="/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
+            DESIGN <img src="/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
+            INSPIRE <img src="/projectImage/Star.png" alt=""  className='marque-star1 w-[7vw]'/>
         </span>
          </div>
 
          <div className='whitespace-nowrap animate-revers font-heading  text-6x lg:text-[7vw] leading-[1]'> 
         <span className='  font-bold flex items-center gap-4'>
-            CREATE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
-            DESIGN <img src="src/assets/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
-            INSPIRE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
-            CREATE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
-            DESIGN <img src="src/assets/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
-            INSPIRE <img src="src/assets/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
+            CREATE <img src="/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
+            DESIGN <img src="/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
+            INSPIRE <img src="/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
+            CREATE <img src="/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
+            DESIGN <img src="/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
+            INSPIRE <img src="/projectImage/Star.png" alt=""  className='marque-revers w-[7vw]'/>
         </span>
         
          </div>
