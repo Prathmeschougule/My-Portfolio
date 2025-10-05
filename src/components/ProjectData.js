20248a49ac2fd55b30e7aa6019f072f6acca2e85
Prathmesh Chougule <prathmeshuix@gmail.com>
2025-10-05 17:15:33 +0530

    d

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -2,28 +2,28 @@ const projects= [
     {
         id:1,
         name:"saloon Booking",
-        image:"src/assets/projectImage/download.jpeg",
+        image:"projectImage/download.jpeg",
         link:"https://github.com/Prathmeschougule/Saloon-Booking-Project",
         description:"The Saloon Booking Project is a full-stack application built with a microservices architecture. It allows users to book salon services, while salon owners can manage their services, track bookings, and monitor revenue. The system ensures secure authentication, real-time notifications, and seamless payment integration."
     },
     {
         id:2,
         name:"e-commerce",
-        image:"src/assets/projectImage/images (3).jpeg",
+        image:"projectImage/images (3).jpeg",
         link:"https://github.com/Prathmeschougule/e-commerce-project",
         description:"A modern full-stack eCommerce web application built using Spring Boot, React.js, and deployed on AWS. The application supports user authentication, product listing, secure checkout, and admin product management."
     },
     {
         id:3,
         name:"AI-Fitness",
-        image:"src/assets/projectImage/images (4).jpeg",
+        image:"projectImage/images (4).jpeg",
         link:"https://github.com/Prathmeschougule/AI-Fitness-Advisor",
         description:"Welcome to the AI-Powered Fitness App! This application is designed with a microservices architecture using Spring Boot and integrates AI through the Google Gemini API for personalized fitness insights."
     },
     {
         id:4,
         name:"saloon Booking",
-        image:"src/assets/projectImage/download.jpeg",
+        image:"projectImage/download.jpeg",
         link:"https://github.com/Prathmeschougule/Saloon-Booking-Project",
                 description:"The Saloon Booking Project is a full-stack application built with a microservices architecture. It allows users to book salon services, while salon owners can manage their services, track bookings, and monitor revenue. The system ensures secure authentication, real-time notifications, and seamless payment integration."
 
@@ -31,7 +31,7 @@ const projects= [
     {
         id:5,
         name:"e-commerce",
-        image:"src/assets/projectImage/images (3).jpeg",
+        image:"projectImage/images (3).jpeg",
         link:"https://github.com/Prathmeschougule/e-commerce-project",
                 description:"A modern full-stack eCommerce web application built using Spring Boot, React.js, and deployed on AWS. The application supports user authentication, product listing, secure checkout, and admin product management."
 
@@ -39,7 +39,7 @@ const projects= [
     {
         id:6,
         name:"AI-Fitness",
-        image:"src/assets/projectImage/images (4).jpeg",
+        image:"projectImage/images (4).jpeg",
         link:"https://github.com/Prathmeschougule/AI-Fitness-Advisor",
         description:"Welcome to the AI-Powered Fitness App! This application is designed with a microservices architecture using Spring Boot and integrates AI through the Google Gemini API for personalized fitness insights."
 
