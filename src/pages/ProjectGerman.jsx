bc740e34df61c566a80ecf85dd1ff01dbeaf9696
Prathmesh Chougule <prathmeshuix@gmail.com>
2026-04-01 23:43:28 +0530

    added german redirect

---
+++ src/pages/ProjectGerman.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectGerman() {
+  return (
+    <div>
+        <h1 className='text-4xl font-bold text-center mt-20'>German</h1>
+    </div>
+  )
+}
+
+export default ProjectGerman
