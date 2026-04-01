99e527fb1159a268e1a1e8197636d37c68e8039e
Prathmesh Chougule <prathmeshuix@gmail.com>
2026-04-02 00:06:47 +0530

    german url done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './pages/Footer'
-import { Route, Routes } from 'react-router-dom'
 import Project from './pages/Project'
 import WhatsApp from './components/WhatsApp';
+import German from './components/German'    // add this import
 
 function App() {
+  const location = useLocation();
+  const hideShell = location.pathname === '/german';
 
   return (
-     <div>
-      <WhatsApp/>
-        <>
-            <Navbar/>
-            <Routes>
-               <Route path='/' element={<Home/>} /> 
-               <Route path='/projects'  element={<Project/>}/> 
-               <Route path='/'  element={<ProjectGerman/>}/>
-            </Routes>
-            <Footer/>
-        </>
+    <div>
+      {!hideShell && <WhatsApp/>}
+      <>
+        {!hideShell && <Navbar/>}
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/projects' element={<Project/>} />
+          <Route path='/german' element={<German/>} />   {/* new route */}
+        </Routes>
+        {!hideShell && <Footer/>}
+      </>
     </div>
   )
 }
---
+++ src/components/German.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function German() {
+  return (
+    <div>
+        <div style={{ height: "100vh" }}>
+            <iframe
+                src="https://german-classes-m8c4.vercel.app/#home"
+                width="100%"
+                height="100%"
+                style={{ border: "none" }}
+            />
+    </div>        
+    </div>
+  )
+}
+
+export default German
